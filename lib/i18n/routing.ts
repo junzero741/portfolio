cf078c1139f1ed902f737809c9e50e114c12683a
준영 <junzero741@gmail.com>
2026-02-17 00:15:52 +0900

    chore: defaultLocale 를 한국어로 변경

--- lib/i18n/routing.ts
+++ lib/i18n/routing.ts
@@ -4,5 +4,5 @@ export const routing = defineRouting({
   // A list of all locales that are supported
   locales: ['en', 'ko'],
   // Used when no locale matches
-  defaultLocale: 'en'
+  defaultLocale: 'ko'
 });
